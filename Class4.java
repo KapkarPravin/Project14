7f87918868f9280489641246879834d047439eb2
KapkarPravin <pravin.kapkar26@gmail.com>
2023-05-18 00:00:50 +0530

    This is class4

---
+++ Class4.java
@@ -0,0 +1,12 @@
+package package4;
+
+public class Class4 {
+	
+	public void name(String name) {
+		System.out.println("name: "+name);
+	}
+	public static void speak() {
+		System.out.println("Animal speaks as per there type");
+
+	}
+}
